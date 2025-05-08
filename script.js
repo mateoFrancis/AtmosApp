d423a9de4bd687c653dea50eebfbbb2a25c7af69
NateWilemon <nathanielwilemon@gmail.com>
2025-05-07 23:58:58 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -414,9 +414,19 @@ function centerMap(lat, long){
 
   
 
-  async function addPin(Lat,Long, bright, time){
+  async function addPin(Lat,Long, bright, time, day){
     const { Map } = await google.maps.importLibrary("maps");
-    let iconURL = 'fire_imgs/OrangeFire.png'
+    let iconURL = '';
+    if (day < 4) {
+        iconURL = 'fire_imgs/RedFire.png'
+        //red
+      } else if (day < 7) {
+         iconURL = 'fire_imgs/OrangeFire.png'
+        //orange
+      } else {
+         iconURL = 'fire_imgs/GreenFire.png'
+        //green
+      } 
     const marker = new google.maps.Marker({
         map: window.map,
         position: { lat: parseFloat(Lat), lng: parseFloat(Long) },
@@ -427,6 +437,8 @@ function centerMap(lat, long){
           },
       });
 
+
+
       //constructs infowindow
   const content = `
   <div class="feh-content">
@@ -434,6 +446,7 @@ function centerMap(lat, long){
  <p class="feh-content-description">Longitude:${Long}</p>
  <p class="feh-content-description">Brightness:${bright}</p>
  <p class="feh-content-description">Reported At:${time}</p>
+ <p class="feh-content-description">Reported ${day} day(s) ago</p>
   </div>
 `;
 
@@ -462,250 +475,4 @@ infoWindow.addListener('closeclick', function() {
 
   
 
-async function makeMarker(Lat, Long, id, humidity, temperature, wind_speed, degree, bright, time, zip, weather ) {
-    const { Map } = await google.maps.importLibrary("maps");
-    // creates a map if none exists
-    if (!window.map) {
-      window.map = new Map(document.getElementById("map"), {
-        center: { lat: 34.0549, lng: -118.2426 },
-        zoom: 8,
-        mapId: 'b32f4d216a605f57',
-      });
-    }
-
-
-
-
-  
-    // calculates color using FFWI 
-    //FFWI = (n*sqr(1+U^2))/0.3002 where u is wind speed
-    //n = 1-2 *(EMC/30) + 1.5* (EMC/30)^2 -0.5(EMC/30)^3
-    //EMC formula changes depending on humidity.
-
-    //Calculating EMC
-    
-    let EMC = 0;
-    if (humidity < 10)
-        EMC = 0.03229+0.281073 * humidity - 0.000578 * humidity * temperature;
-    else if(humidity >= 10 && humidity < 50)
-        EMC = 2.22749 + 0.160107 * humidity - 0.01478 * temperature;
-    else if(humidity >= 50)
-        EMC = 21.0606 + 0.005565 * (humidity*humidity) - 0.00035 * humidity * temperature - 0.483199 * humidity
-
-    //using EMC to calculate n
-    let n = 1 - 2 * (EMC/30) + 1.5 * (EMC/30 * EMC/30) - 0.5 * (EMC/30 * EMC/30 *EMC/30);
-
-    //calculating index FFWI and converting it to 1-5 severity.
-    let FFWI = (n * Math.sqrt(1 + (wind_speed*wind_speed)))/0.3002;
-    console.log(FFWI);
-    //scale of 0-100, where any number greater than 100 is set back to 100
-
-
-
-      //adds data to live updates
-      document.getElementById("live").innerHTML += `At ${time} a severity ${FFWI.toFixed(3)} fire at ${FFWI.toFixed(3)}. &nbsp;&nbsp;&nbsp;`;
-
-    // adds data to the table
-    let newrow = document.createElement('tr');
-
-    newrow.innerHTML = `<td>${id}</td> 
-    <td>${zip}</td>
-    <td>${time}</td>
-     <td>${FFWI.toFixed(3)}</td>
-    <td>${temperature}°F</td>
-    <td>${humidity}%</td>
-    <td>${wind_speed}mph / ${degree}°</td>
-    <td>${weather}</td>`
-
-    let tablebody = document.querySelector('#fireTable tbody');
-    tablebody.appendChild(newrow);
-
-    
-    //Sets severiy for the index
-    let iconURL = '';
-
-    if (FFWI < 21) {
-      iconURL = 'images/green.png'
-      //green
-    } else if (FFWI < 41) {
-      iconURL ='images/blue.png'
-      //blue
-    } else if (FFWI < 61) {
-      iconURL = 'images/yellow.png'
-      //yellow
-    } else if (FFWI < 81) {
-       iconURL = 'images/orange.png'
-      //orange
-    } else if (FFWI >= 81) {
-       iconURL = 'images/red.png'
-      //red
-    } 
-  
-    // Create marker depending on what pin is set to.
-      const marker = new google.maps.Marker({
-      map: window.map,
-      position: { lat: parseFloat(Lat), lng: parseFloat(Long) },
-      title: 'Fire',
-      icon: {
-          url: iconURL,
-          scaledSize: new google.maps.Size(35, 35),
-        },
-    });
-
-
-  //constructs infowindow
-  const content = `
-    <div class="feh-content">
-      <p class="feh-content-description">Fire #${id}</p>
-      <p class="feh-content-description">Latitude: ${Lat}°F </p>
-      <p class="feh-content-description">Longitude: ${Long}% </p>
-      <p class="feh-content-description">Brightness (K): ${bright}% </p>
-      <p class="feh-content-description">Time (PST): ${time} </p>
-      <p class="feh-content-description">Weather: ${weather} </p>
-      <p class="feh-content-description">Severity Index: ${FFWI.toFixed(3)} </p>
-    </div>
-  `;
-  
-  const infoWindow = new google.maps.InfoWindow({
-    content: content,
-    minWidth: 200,
-    maxWidth: 200,
-  });
-  window.currentInfoWindow = infoWindow;
-    //removes old info-window if you click a new one.
-    marker.addListener('click', () => {
-      //close old info window if new one opens
-      if (window.currentInfoWindow) {
-          window.currentInfoWindow.close();
-        }
-    infoWindow.setContent(content);
-    infoWindow.open(window.map, marker);
-    //track info windows to close old one
-    window.currentInfoWindow = infoWindow;
-  });
-
-  infoWindow.addListener('closeclick', function() {
-    window.map.fitBounds(window.bounds);
-  });
-  }
-
-
-
-
-
-
-
-
-
-
-//The exact same function as before but It takes an array of arrays as an input.
-//Formated as:
-//  array(Latitude, Longtitude, Color, Title, Description)
-
-async function array_makeMarkers(markersData) {
-    const { Map } = await google.maps.importLibrary("maps");
-  
-    // Creates a map if none exists
-    if (!window.map) {
-      window.map = new Map(document.getElementById("map"), {
-        center: { lat: 34.0549, lng: -118.2426 },
-        zoom: 8,
-        mapId: 'b32f4d216a605f57',
-      });
-    }
-   
-
-  
-    // Loop through each marker data and create the markers
-    markersData.forEach(([Lat, Long, Color, Title, Description]) => {
-      let iconURL = '';
-      // calculates color using FFWI 
-    //FFWII = (n*sqr(1+U^2))/0.3002 where u is wind speed
-    //n = 1-2 *(EMC/30) + 1.5* (EMC/30)^2 -0.5(EMC/30)^3
-    //EMC formula changes depending on humidity.
-
-    //Calculating EMC
-    let EMC = 0;
-    if (humidity < 10)
-        EMC = 0.03229+0.281073 * humidity - 0.000578 * humidity * temperature;
-    else if(humidity >= 10 && humidity < 50)
-        EMC = 2.22749 + 0.160107 * humidity - 0.01478 * temperature;
-    else if(humidity >= 50)
-        EMC = 21.0606 + 0.005565 * (humidity*humidity) - 0.00035 * humidity * temperature - 0.483199 * humidity
-
-    //using EMC to calculate n
-    let n = 1 - 2 * (EMC/30) + 1.5 * (EMC/30 * EMC/30) - 0.5 * (EMC/30 * EMC/30 *EMC/30);
-
-    //calculating index FFWI and converting it to 1-5 severity.
-    let FFWI = (n * Math.sqrt(1 + (wind_speed*wind_speed)))/0.3002;
-    //scale of 0-100, where any number greater than 100 is set back to 100
-
-
-    
-      if (FFWI > 21) {
-        iconURL = 'images/green.png';
-      } else if (FFWI > 41) {
-        iconURL = 'images/blue.png';
-      } else if (FFWI > 61) {
-        iconURL = 'images/yellow.png';
-      } else if (FFWI > 81) {
-        iconURL = 'images/orange.png';
-      } else if (FFWI >= 81) {
-        iconURL = 'images/red.png';
-      } 
-
-      
-      const marker = new google.maps.Marker({
-        map: window.map,
-        position: { lat: Lat, lng: Long },
-        title: Title,
-        icon: {
-          url: iconURL,
-          scaledSize: new google.maps.Size(50, 50),
-        },
-      });
-    
-      const infoWindow = new google.maps.InfoWindow({
-        minWidth: 200,
-        maxWidth: 200,
-      });
-    
-      const content = `
-        <div class="feh-content">
-          <h3 class="feh-content-title">${Title}</h3>
-          <p class="feh-content-description">${Description}</p>
-        </div>`;
-    
-      marker.addListener('click', () => {
-          //close old info window if new one opens
-          if (window.currentInfoWindow) {
-              window.currentInfoWindow.close();
-            }
-        infoWindow.setContent(content);
-        infoWindow.open(window.map, marker);
-        //track info windows to close old one
-        window.currentInfoWindow = infoWindow;
-      });
-  
-  
-      /* Commented out because I don't think it works well with our project
-      //changes map to zoom out to display all markers
-      if (!window.bounds) {
-          window.bounds = new google.maps.LatLngBounds();
-        }
-        // Add the new marker's position to the bounds
-      window.bounds.extend(new google.maps.LatLng(parseFloat(Cords[0]), parseFloat(Cords[1])));
-      
-      // Adjust the map to fit the new bounds
-      window.map.fitBounds(window.bounds);
-      //end of changing map position, probably remove
-      */
-  
-  
-      //handle when the infoWindow is closed
-      infoWindow.addListener('closeclick', function() {
-        window.map.fitBounds(window.bounds);
-      });
-    });
-  }  
 
